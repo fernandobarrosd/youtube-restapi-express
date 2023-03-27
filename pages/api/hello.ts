5bc1ab529ea7444532640ad6bafe280d995a7c2a
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2023-03-27 16:14:00 -0300

    Edit project

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,4 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { youtubeAPI } from "../../src/services/youtube-api-service";
-
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     res.status(201).send("Youtube RestApi Express");
-    console.log(youtubeAPI);
 }
--- src/services/youtube-api-service.ts
+++
@@ -1,8 +0,0 @@
-import { YoutubeDataAPI } from "youtube-v3-api";
-import { config } from "dotenv";
-
-config();
-
-const { YOUTUBE_API_KEY } = process.env;
-
-export const youtubeAPI = new YoutubeDataAPI(YOUTUBE_API_KEY);
